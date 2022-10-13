d60a0b191081b80a0d574e4bf3ad90e67e87a1d4
makwaneshriya <shriyamakwane77@gmail.com>
2022-10-13 22:20:03 +0530

    adding updated new.java

--- new.java
+++ new.java
@@ -4,6 +4,7 @@ class A
                                                                    A-new B ----- m1() -m1()  m1|(int) m1 (int int)
   void m1(int);
 }                                                                   D = new A    --- a.m1()
+
 class B extends A
 {
   void m1();
